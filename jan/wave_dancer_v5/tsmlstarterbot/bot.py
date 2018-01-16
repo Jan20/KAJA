6a8069deacf38aa761708371fc6f7ebd9bed7440
Jan Schumann <Jan-Philip.Schumann@gmx.de>
2018-01-16 20:46:07 +0100

    wave_dance_v5

--- jan/wave_dancer_v5/tsmlstarterbot/bot.py
+++ jan/wave_dancer_v5/tsmlstarterbot/bot.py
@@ -5,8 +5,140 @@ import time
 import logging
 
 import hlt
+
+import sys
+
 from tsmlstarterbot.common import *
 from tsmlstarterbot.neural_net import NeuralNet
+
+
+
+#####################
+### Ugly solution ###
+#####################
+
+# def get_closest_enemy_ships(self, ship):
+#     all_enemy_ships = [enemy_ship for enemy_ship in game_map._all_ships() if enemy_ship not in team_ships and enemy_ship.id != ship.id]
+#     enemy_ships_nearby = [enemy_ship for enemy_ship in game_map._all_ships() if enemy_ship not in initial_team_ships and ship.calculate_distance_between(enemy_ship) < 30]
+
+
+# class Flock:
+    
+#     def __init__(self, members, closest_enemy_ships):
+#         self.members = members
+#         self.closest_enemy_ships
+
+
+#     initial_team_ships = [team_ship for team_ship in game_map.get_me().all_ships()]
+#     enemy_ships_nearby = [enemy_ship for enemy_ship in game_map._all_ships() if enemy_ship not in initial_team_ships and ship.calculate_distance_between(enemy_ship) < 40]
+#     closest_enemy_ships = []
+
+#     current -> array[Enemy_ships]
+
+#     self.members
+#         initial_team_ships = [team_ship for team_ship in game_map.get_me().all_ships()]
+#         team_ships = []
+#         for team_ship in initial_team_ships:
+#             if team_ship.id != ship.id:
+#                 team_ships.append(team_ship)
+
+#         team_ships_nearby = [team_ship for team_ship in team_ships if ship.calculate_distance_between(team_ship) < 50]
+        
+#         closest_enemy_ship_distance = 1000
+#         closest_enemy_ship = all_enemy_ships[0]
+
+#     def get_closest_enemy_ship:
+#         self.members[0].calculate_distance_between(closest_enemy_ships[0])
+
+
+
+class Entity:
+   
+
+    def __init__(self, x, y, radius, health, player, entity_id):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.health = health
+        self.owner = player
+        self.id = entity_id
+
+    def calculate_distance_between(self, target):
+        """
+        Calculates the distance between this object and the target.
+        :param Entity target: The target to get distance to.
+        :return: distance
+        :rtype: float
+        """
+        return math.sqrt((target.x - self.x) ** 2 + (target.y - self.y) ** 2)
+
+    def calculate_angle_between(self, target):
+        """
+        Calculates the angle between this object and the target in degrees.
+        :param Entity target: The target to get the angle between.
+        :return: Angle between entities in degrees
+        :rtype: float
+        """
+        return math.degrees(math.atan2(target.y - self.y, target.x - self.x)) % 360
+
+    def closest_point_to(self, target, min_distance=3):
+        """
+        Find the closest point to the given ship near the given target, outside its given radius,
+        with an added fudge of min_distance.
+        :param Entity target: The target to compare against
+        :param int min_distance: Minimum distance specified from the object's outer radius
+        :return: The closest point's coordinates
+        :rtype: Position
+        """
+        angle = target.calculate_angle_between(self)
+        radius = target.radius + min_distance
+        x = target.x + radius * math.cos(math.radians(angle))
+        y = target.y + radius * math.sin(math.radians(angle))
+
+        return Position(x, y)
+
+    def _link(self, players, planets):
+        pass
+
+    def __str__(self):
+        return "Entity {} (id: {}) at position: (x = {}, y = {}), with radius = {}"\
+            .format(self.__class__.__name__, self.id, self.x, self.y, self.radius)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+
+class Position(Entity):
+    """
+    A simple wrapper for a coordinate.
+
+    Intended to be passed to some functions in place of a ship or planet.
+    :ivar id: Unused
+    :ivar x: The x-coordinate.
+    :ivar y: The y-coordinate.
+    :ivar radius: The position's radius (should be 0).
+    :ivar health: Unused.
+    :ivar owner: Unused.
+    """
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.radius = 0
+        self.health = None
+        self.owner = None
+        self.id = None
+
+    def _link(self, players, planets):
+        raise NotImplementedError("Position should not have link attributes.")
+
+
+class FuturePosition:
+    def __init__(self, ship_id, Position):
+        self.ship_id = ship_id
+        self.Position = Position
+
 
 class Bot:
     def __init__(self, location, name):
@@ -113,6 +245,28 @@ class Bot:
 
         return feature_matrix
 
+
+    ###########################
+    ### Collision Avoidance ###
+    ###########################
+    def compute_future_position(self, active_ship, target):
+        
+        delta_x = 0
+        delta_y = 0
+        delta_z = active_ship.calculate_distance_between(target)
+
+        alpha = active_ship.calculate_angle_between(target)
+        beta = 90 - alpha
+        gamma = 90
+
+        delta_x = math.cos(alpha) * delta_z
+        delta_y = math.sin(beta) * delta_z
+        new_position = Position(x = active_ship.x + delta_x, y = active_ship.y + delta_y)
+        return new_position
+    ##################################
+    ### End of Collision Avoidance ###
+    ##################################
+
     def produce_ships_to_planets_assignment(self, game_map, predictions):
         """
         Given the predictions from the neural net, create assignment (undocked ship -> planet) deciding which
@@ -197,17 +351,53 @@ class Bot:
         :return: list of instructions to send to the Halite engine
         """
         command_queue = []
+        future_positions = []
         # Send each ship to its planet
 
         ######################################################
         ### Undock Mechanic in case of a nearby enemy ship ###
         ######################################################
         
-
+        # Kind of useless
 
         ##############################
         ### End of Undock Mechanic ###
         ##############################
+
+
+        ################
+        ### Flocking ###
+        ################
+        for ship, planet in ships_to_planets_assignment:
+    
+            logging.info("ship.position")
+            logging.info(ship.x)
+            logging.info(ship.y)
+            target = Position(ship.x, ship.y)
+
+            logging.info('New Position')
+            logging.info(target)
+
+
+            logging.info('--------------------------------------Flocking-----------------------------------------------')
+            logging.info(ship.calculate_angle_between(Position(0, 0)))
+
+            future_position_map = []
+            # newFuturePosition = self.get(ship, Position(0,0))
+            #new_future_position = FuturePosition(ship.id, Position(ship.x + delta_x, ship.y + delta_y))
+
+            logging.info("New Position")
+            #logging.info(future_position_map[0].Position)
+            # for ship, planet in ships_to_planets_assignment:
+            #     if ship.x == target.x and ship.y == target.y:
+            #         new_target = 
+            #         logging.info('--------------------------------------Awesome there is one -----------------------------------------------')
+    
+
+        #######################
+        ### End of Flocking ###
+        #######################
+
         target_planet = None
         filtered_planets_by_distance = []
         distance_to_next_planet = 1000
@@ -291,11 +481,22 @@ class Bot:
 
                 logging.info('Combat Mechanic Active______________________')
                 logging.info(closest_team_ship_distance)
-                if closest_team_ship_distance > 11: 
+                if closest_team_ship_distance > 2: 
                     if closest_team_ship != None:
-                        logging.info('Defend________________________________________________')
+                        logging.info('Find a friend')
+
+                        navigation_target = Position(closest_team_ship.x + 0.6, closest_team_ship.y + 0.6)
+                        
+                        for future_position in future_positions:
+                            if future_position.x == navigation_target.x and future_position.y == navigation_target.y:
+                                navigation_target = Position(closest_team_ship.x + 0.6, closest_team_ship.y + 0.6)
+                        future_position = self.compute_future_position(ship, navigation_target)
+                        
+                        future_positions.append(future_position)
+
                         command_queue.append(
-                            self.navigate(game_map, round_start_time, ship, closest_team_ship, speed))
+                            self.navigate(game_map, round_start_time, ship, navigation_target, speed))
+
                 else:
                     if closest_enemy_ship != None:
                         command_queue.append(
@@ -345,7 +546,7 @@ class Bot:
                     self.navigate(game_map, round_start_time, ship, ship.closest_point_to(weakest_ship), speed))
         return command_queue
 
-    def navigate(self, game_map, start_of_round, ship, destination, speed):
+    def navigate(self, game_map, start_of_round, ship, destination,  speed):
         """
         Send a ship to its destination. Because "navigate" method in Halite API is expensive, we use that method only if
         we haven't used too much time yet.
